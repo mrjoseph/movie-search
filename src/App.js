cc1a3dcb3ee10534bcf858d4e7a7eabe6fc3dde3
Trevor Joseph <trevor.joseph@johnlewis.co.uk>
2018-07-17 06:57:58 +0100

    added react bootstrap and redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Jumbotron, PageHeader } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <PageHeader>
+              Movie Search
+          </PageHeader>;
+          <div className="container">
+              <Jumbotron>
+              <h1>Hello, world!</h1>
+              <p>
+                  This is a simple hero unit, a simple jumbotron-style component for calling
+                  extra attention to featured content or information.
+              </p>
+              <p>
+                  <Button bsStyle="primary">Learn more</Button>
+              </p>
+          </Jumbotron>
+          <Button bsStyle="success">Success</Button>
+          </div>
       </div>
     );
   }
